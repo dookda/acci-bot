c296e4e4801236a7347f894648d839a97f59c338
sakda homhuan <sakda.homhuan@gmail.com>
2023-10-30 07:24:49 +0700

    update firebase

---
+++ public/add/app.js
@@ -0,0 +1,182 @@
+$(document).ready(async () => {
+
+    // await liff.init({ liffId: "1654018081-pemA61EA" })
+    // liff.ready.then(async () => {
+    //     if (!liff.isLoggedIn()) {
+    //         liff.login()
+    //     }
+    //     await getUserid()
+    // })
+    loadMap();
+});
+
+var userId;
+async function getUserid() {
+    const profile = await liff.getProfile();
+    console.log(profile)
+    userId = await profile.userId;
+
+    $('#profile').attr('src', await profile.pictureUrl);
+    // $('#userId').text(profile.userId);
+    $('#statusMessage').text(await profile.statusMessage);
+    $('#displayName').text(await profile.displayName);
+}
+
+var map = L.map('map', {
+    center: [16.820378, 100.265787],
+    zoom: 13
+});
+var marker, gps, dataurl, tam, amp, pro, x, y;
+var url = 'https://rti2dss.com/p3100';
+// var url = 'http://localhost:3100';
+
+function loadMap() {
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    const grod = L.tileLayer('https://{s}.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    const ghyb = L.tileLayer('https://{s}.google.com/vt/lyrs=y,m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    var prov = L.tileLayer.wms("https://rti2dss.com:8443/geoserver/th/wms?", {
+        layers: 'th:province_4326',
+        format: 'image/png',
+        transparent: true
+    });
+    var baseMap = {
+        "OSM": osm,
+        "แผนที่ถนน": grod,
+        "แผนที่ภาพถ่าย": ghyb.addTo(map)
+    }
+    var overlayMap = {
+        "ขอบจังหวัด": prov.addTo(map)
+    }
+    L.control.layers(baseMap, overlayMap).addTo(map);
+}
+
+function onLocationFound(e) {
+    x = e.latlng.lat;
+    y = e.latlng.lng;
+    gps = L.marker(e.latlng, { draggable: true });
+    // gps.addTo(map).bindPopup("คุณอยู่ที่นี่").openPopup();
+    // gps.on('dragend', (e) => {
+    //     console.log(e)
+    // })
+    $.get(`/acc-api/getaddress/${x}/${y}`).done((res) => {
+        tam = res.data[0].tam_name;
+        amp = res.data[0].amp_name;
+        pro = res.data[0].pro_name;
+        // const txt = `ต.${tam} อ.${amp} จ.${pro}`;
+        // $('#address').val(txt)
+        $('#tam').val(tam);
+        $('#amp').val(amp);
+        $('#pro').val(pro);
+    })
+}
+
+function onLocationError(e) {
+    console.log(e.message);
+}
+
+function refreshPage() {
+    location.reload(true);
+}
+
+// var canvas = document.getElementById("canvas");
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var cw = canvas.width;
+var ch = canvas.height;
+var maxW = 640;
+var maxH = 640;
+
+var input = document.getElementById('imgfile');
+var output = document.getElementById('preview');
+input.addEventListener('change', handleFiles);
+
+function handleFiles(e) {
+    var img = new Image;
+    img.onload = async function () {
+        var iw = img.width;
+        var ih = img.height;
+        var scale = Math.min((maxW / iw), (maxH / ih));
+        var iwScaled = iw * scale;
+        var ihScaled = ih * scale;
+        canvas.width = iwScaled;
+        canvas.height = ihScaled;
+        ctx.drawImage(img, 0, 0, iwScaled, ihScaled);
+        dataurl = await canvas.toDataURL("image/jpeg", 0.5);
+        // console.log(dataurl)
+        $("#preview").attr("src", dataurl);
+    }
+    img.src = URL.createObjectURL(e.target.files[0]);
+    // console.log(img)
+}
+
+map.on('locationfound', onLocationFound);
+// map.on('locationerror', onLocationError);
+// map.locate({ setView: true, maxZoom: 18 });
+
+var i = 1;
+$("#addMore").click(function (e) {
+    i += 1;
+    console.log(e)
+    e.preventDefault();
+    $("#fieldList").append(`<br><label for="patient">ชื่อ ผู้บาดเจ็บรายที่ ${i}:</label>
+    <input type="text" name="name[]" class="form-control" />`);
+});
+
+var lc = L.control.locate({
+    position: 'topleft',
+    strings: {
+        title: ""
+    },
+    locateOptions: {
+        enableHighAccuracy: true,
+    }
+}).addTo(map);
+
+lc.start();
+
+function saveData() {
+    $("#status").empty().text("File is uploading...");
+    var name = [];
+    $('input[name^=name]').each(function () {
+        name.push($(this).val());
+    });
+
+    if (!dataurl) {
+        dataurl = '-';
+    }
+
+    // alert(dataurl)
+    const obj = {
+        name: name,
+        acc_place: $('#location').val(),
+        tam: $('#tam').val(),
+        amp: $('#amp').val(),
+        pro: $('#pro').val(),
+        x: x,
+        y: y,
+        vehicle: $('#vehicle').val(),
+        img: dataurl,
+        geom: JSON.stringify(gps.toGeoJSON().geometry)
+    }
+
+    $.post('/acc-api/lineinsert', obj).done((res) => {
+        if (res.status == 'success') {
+            closed();
+        }
+        $('form :input').val('');
+        $('#preview').attr('src', '');
+    })
+};
+
+function closed() {
+    // liff.closeWindow();
+}
---
+++ public/risk/risk.js
@@ -0,0 +1,159 @@
+
+$(document).ready(function () {
+    loadMap();
+})
+
+let latlng = {
+    lat: 16.820378,
+    lng: 100.265787
+};
+let map = L.map("map", {
+    center: latlng,
+    zoom: 13
+});
+
+var marker;
+
+function loadMap() {
+    const Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    const grod = L.tileLayer('https://{s}.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    const ghyb = L.tileLayer('https://{s}.google.com/vt/lyrs=y,m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+
+    const pro = L.tileLayer.wms("https://rti2dss.com:8443/geoserver/th/wms?", {
+        layers: 'th:province_4326',
+        format: 'image/png',
+        transparent: true,
+        zIndex: 5,
+        CQL_FILTER: 'pro_code=53'
+
+    });
+    const amp = L.tileLayer.wms('https://rti2dss.com:8443/geoserver/th/wms?', {
+        layers: 'th:amphoe_4326',
+        format: 'image/png',
+        transparent: true,
+        zIndex: 5,
+        CQL_FILTER: 'pro_code=53'
+    });
+    const tam = L.tileLayer.wms('https://rti2dss.com:8443/geoserver/th/wms?', {
+        layers: 'th:tambon_4326',
+        format: 'image/png',
+        transparent: true,
+        zIndex: 5,
+        CQL_FILTER: 'pro_code=53'
+    });
+
+    const riskpoint = L.tileLayer.wms('https://rti2dss.com:8443/geoserver/wms?', {
+        layers: 'acci:ud_riskpoint_4326_v',
+        format: 'image/png',
+        transparent: true,
+        zIndex: 5
+    });
+
+    var baseMap = {
+        "OSM": Mapnik,
+        "แผนที่ถนน": grod,
+        "แผนที่ภาพถ่าย": ghyb.addTo(map)
+    }
+
+    var overlay = {
+        'ขอบเขตตำบล': tam.addTo(map),
+        'ขอบเขตอำเภอ': amp.addTo(map),
+        'ขอบเขตจังหวัด': pro.addTo(map),
+        'จุดเสี่ยง': riskpoint
+    }
+
+    layerControl = L.control.layers(baseMap, overlay).addTo(map);
+    // layerControl.addOverlay(tam.addTo(map), 'ขอบเขตตำบล');
+    // layerControl.addOverlay(amp.addTo(map), 'ขอบเขตอำเภอ');
+    // layerControl.addOverlay(pro.addTo(map), 'ขอบเขตจังหวัด');
+    // layerControl.addOverlay(riskpoint.addTo(map), 'จุดเสี่ยง');
+
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
+    // map.locate({ setView: true, watch: false, maxZoom: 16 });
+}
+
+var lc = L.control.locate({
+    position: 'topleft',
+    locateOptions: {
+        enableHighAccuracy: true,
+    }
+}).addTo(map);
+
+lc.start();
+
+var latlon;
+
+function onLocationFound(e) {
+    console.log(e)
+    latlon = e.latlng;
+    getDisease(e.latitude, e.longitude)
+}
+
+function onLocationError(e) {
+    console.log(e.message);
+}
+
+function getDisease(lat, lon) {
+    var point = L.layerGroup();
+    var buff = 1000;
+    const icon = './../img/caution.svg';
+    const iconMarker = L.icon({
+        iconUrl: icon,
+        iconSize: [30, 30],
+        iconAnchor: [15, 20],
+        popupAnchor: [5, -36]
+    });
+    map.eachLayer((lyr) => {
+        // console.log(lyr);
+        if (lyr.options.iconName == 'risk') {
+            map.removeLayer(lyr);
+        }
+    });
+    // lat = 17.629797316684733;
+    // lon = 100.12873467982418;
+    $.get(`https://rti2dss.com:3100/acc-api/riskpoint/${lat}/${lon}/${buff}`, (res) => {
+        $('#sumpoint').text('พบจุดเสี่ยงใกล้คุณ ' + res.count + ' จุด');
+        $('#items').empty();
+        // console.log(res)
+        let marker = L.geoJSON(res.data, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {
+                    icon: iconMarker,
+                    iconName: 'risk',
+                    attribute: feature.properties
+                });
+            },
+            onEachFeature: (feature, layer) => {
+                console.log(feature);
+                if (feature.properties) {
+                    layer.bindPopup(feature.properties.sname);
+                }
+                var newDiv = $(`<h4><span class="badge badge-warning">${feature.properties.stype} ${feature.properties.sname}</span></h4>`);
+                // console.log(feature.properties)
+                $('#items').append(newDiv);
+            }
+        });
+        marker.addTo(point);
+        point.addTo(map);
+    })
+    // layerControl.addOverlay(point.addTo(map), 'จุดเสี่ยงในรัศมี 2 กม.');
+}
+
+
+
+
+
+
+
+
